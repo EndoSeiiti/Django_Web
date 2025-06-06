06a3d11f9b7d1a8a4f32ea759f23647daa5263ea
endoseiiti <endoseiiti@gmail.com>
2025-06-06 15:54:19 -0300

    feat:added create task feature

--- Website/myapp/models.py
+++ Website/myapp/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class TodoItem(models.Model):
     title= models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description= models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- Website/myapp/views.py
+++ Website/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from.models import TodoItem
+from django.contrib.auth.decorators import login_required
+from .forms import taskform
 
 # Create your views here.
 def home(request):
@@ -8,3 +10,13 @@ def home(request):
 def todos(request):
     items = TodoItem.objects.all()
     return render(request,"todos.html",{"todos":items} )
+
+def create_task(request):
+    if request.method == 'POST':
+        form = taskform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('todos')
+    else:
+        form = taskform()
+    return render(request, "createtask.html", {'form':form})        
